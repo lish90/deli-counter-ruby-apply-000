907431b04b60e9ecda269585000986a5f0550b41
Alisha Musamabik <alisha.musamabik@gmail.com>
2018-12-06 21:31:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+
+
+
+
+def line 
+  katz_deli = []
+  puts"The line is currently empty."
+end
+
+ def take_a_number (katz_deli, String = "#{name}")
+   puts "#{name} "
+ end 
