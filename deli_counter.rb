2425bbb0ca2f920047291e09a1f4473acd42816b
Alisha Musamabik <alisha.musamabik@gmail.com>
2018-12-08 18:25:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,28 @@
 # Write your code here.
+def line 
+   katz_deli = []
+   
+katz_deli.each do |customers| 
+  if katz_deli 
+    puts"The line is currently empty."
+else
+  puts"#{name} you are #{customers} in the line " 
+end
+
+
+
+ def take_a_number (katz_deli, String = "#{name}")
+     "#{name}".push
+     
+     customers.each do |customer| puts "#{name}, you are "#{customer}" 
+ end 
+
+
+ 
+ def now_serving
+   take_a_number.shift
+   if   katz_deli
+     puts "There is nobody waiting to be served!"
+  else 
+    puts take_a_number
+ end 
